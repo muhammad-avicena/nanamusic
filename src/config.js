48cda855f032bbced2848e93dce82dbc21d370bb
Hideaki-Ryu <49929404+hideaki-ryu@users.noreply.github.com>
2022-01-30 11:58:53 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "OTM3MTcxMTQyMTkwNDMyMjU3.YfX2pA.8C9wFWwpZwYu3a86o-7b8PqzFik",  // your bot token
-    prefix: process.env.PREFIX || "n!", // bot prefix
+    token: process.env.TOKEN || "",  // your bot token
+    prefix: process.env.PREFIX || "!", // bot prefix
     ownerID: process.env.OWNERID || "234301642768384010", //your discord id
-    mongourl: process.env.MONGO_URI || "mongodb://newuser:newuser@cluster0-shard-00-00.uf6th.mongodb.net:27017,cluster0-shard-00-01.uf6th.mongodb.net:27017,cluster0-shard-00-02.uf6th.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-6cm745-shard-0&authSource=admin&retryWrites=true&w=majority", // MongoDb URL
+    mongourl: process.env.MONGO_URI || "", // MongoDb URL
     embedColor: process.env.COlOR || "#303236", // embed colour
-    logs: process.env.LOGS || "836641517438763087", // channel id for guild create and delete logs
+    logs: process.env.LOGS || "", // channel id for guild create and delete logs
 
     nodes: [
     {
